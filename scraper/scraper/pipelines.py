ebd03d4a0294725b8e733f5f028f5cf0ce29625f
peterwade153 <peterwade153@gmail.com>
2020-05-25 10:05:20 +0300

    add docs

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -8,12 +8,18 @@
 from word2number import w2n
 
 class ScraperPipeline(object):
+    """
+    Saves Item to the database
+    """
     def process_item(self, item, spider):
         item.save()
         return item
 
 
 class PropertyStatusPipeline(object):
+    """
+    Replace text for item status i.e For Rent will be replaced with Rent.
+    """
     def process_item(self, item, spider):
         if item.get('status'):
             item['status'] = item['status'].replace('For ', '')
@@ -21,6 +27,9 @@ class PropertyStatusPipeline(object):
 
 
 class PropertyPricePipeline(object):
+    """
+    Removes signs from the price value. i.e replaces 10000/= with 10000
+    """
     def process_item(self, item, spider):
         if item.get('price'):
             item['price'] = item['price'].replace('/=', '')
@@ -28,6 +37,9 @@ class PropertyPricePipeline(object):
 
 
 class ConvertNumPipeline(object):
+    """
+    Converts words to number values for bedrooms and bathrooms
+    """
     def process_item(self, item, spider):
         if item.get('bathrooms'):
             item['bathrooms'] = w2n.word_to_num(item['bathrooms'])
